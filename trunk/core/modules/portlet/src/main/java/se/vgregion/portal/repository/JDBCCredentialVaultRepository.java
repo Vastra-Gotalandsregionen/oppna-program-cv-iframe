3dc90f5d8db1faa46e82659ab444d24748208828
wmdaros <wmdaros@47b682a8-c94b-11de-9e96-3193d784f102>
2009-11-10 07:49:03 +0000

    Refactored

--- trunk/core/modules/portlet/src/main/java/se/vgregion/portal/repository/JDBCCredentialVaultRepository.java
+++ trunk/core/modules/portlet/src/main/java/se/vgregion/portal/repository/JDBCCredentialVaultRepository.java
@@ -1,4 +1,4 @@
-package se.vgregion.portal.iframe.repository;
+package se.vgregion.portal.repository;
 
 import org.springframework.jdbc.core.simple.SimpleJdbcDaoSupport;
 import org.springframework.jdbc.core.simple.ParameterizedBeanPropertyRowMapper;
@@ -7,30 +7,42 @@ import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.dao.DataAccessException;
+import org.springframework.core.io.DefaultResourceLoader;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
-import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.beanutils.BeanUtils;
 import se.vgregion.portal.iframe.model.UserSiteCredential;
 import se.vgregion.portal.iframe.util.CryptoUtils;
+import se.vgregion.portal.repository.CredentialVaultRepository;
 
 import javax.sql.DataSource;
-import java.lang.reflect.InvocationTargetException;
 import java.io.File;
 import java.io.IOException;
 import java.io.FileWriter;
-import java.io.FileReader;
 import java.security.GeneralSecurityException;
 import java.util.Properties;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * @author <a href="mailto:david.rosell@redpill-linpro.com">David Rosell</a>
  */
 public class JDBCCredentialVaultRepository extends SimpleJdbcDaoSupport implements CredentialVaultRepository {
     private static Logger log = LoggerFactory.getLogger(JDBCCredentialVaultRepository.class);
-    private static String CREDENTIAL_VAULT_KEY_FILE = "/Users/david/AppServer/liferay-portal-5.2.3/tomcat-6.0.18/webapps/cv-iframe-portlet/WEB-INF/cv.key";
-    private static String DB_CREDENTIAL_FILE = "/Users/david/AppServer/liferay-portal-5.2.3/tomcat-6.0.18/webapps/cv-iframe-portlet/WEB-INF/db_credentials.properties";
+    private String credentialVaultKeyFile;
+    private String dbCredentialFile;
+    private File cvKeyFile;
 
+    public void setCvKeyFile(File cvKeyFile) {
+        this.cvKeyFile = cvKeyFile;
+    }
+
+    public void setCredentialVaultKeyFile(String credentialVaultKeyFile) {
+        this.credentialVaultKeyFile = credentialVaultKeyFile;
+    }
+
+    public void setDbCredentialFile(String dbCredentialFile) {
+        this.dbCredentialFile = dbCredentialFile;
+    }
 
     public UserSiteCredential getUserSiteCredential(String uid, String siteKey) {
         String sql = "select * from usersitecredential where uid = ? and sitekey = ?";
@@ -63,14 +75,15 @@ public class JDBCCredentialVaultRepository extends SimpleJdbcDaoSupport implemen
 
     private void decryptSitePwd(UserSiteCredential creds) throws GeneralSecurityException, IOException {
         String encryptedPwd = creds.getSitePassword();
-        String clearPwd = CryptoUtils.decrypt(encryptedPwd, new File(CREDENTIAL_VAULT_KEY_FILE));
+        String clearPwd = CryptoUtils.decrypt(encryptedPwd, cvKeyFile);
+        log.debug("cvKeyFile path {}", cvKeyFile.getAbsolutePath());
         creds.setSitePassword(clearPwd);
     }
 
     private void encryptSitePwd(UserSiteCredential siteCredential) {
         try {
             String clearPwd = siteCredential.getSitePassword();
-            String encryptedPwd = CryptoUtils.encrypt(clearPwd, new File(CREDENTIAL_VAULT_KEY_FILE));
+            String encryptedPwd = CryptoUtils.encrypt(clearPwd, cvKeyFile);
             siteCredential.setSitePassword(encryptedPwd);
         } catch (GeneralSecurityException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
@@ -82,7 +95,7 @@ public class JDBCCredentialVaultRepository extends SimpleJdbcDaoSupport implemen
     private String insertOrUpdateSql(UserSiteCredential siteCredential) {
         String sql;
         if(siteUserExists(siteCredential)) {
-            sql = "UPDATE USERSITECREDENTIALS SET SITEUSER = :siteUser,  SITEPASSWORD = :sitePassword WHERE UID = :uid AND SITEKEY = :siteKey";
+            sql = "UPDATE USERSITECREDENTIAL SET SITEUSER = :siteUser,  SITEPASSWORD = :sitePassword WHERE UID = :uid AND SITEKEY = :siteKey";
         } else {
             sql = "INSERT INTO USERSITECREDENTIAL (UID, SITEKEY, SITEUSER, SITEPASSWORD) VALUES(:uid, :siteKey, :siteUser, :sitePassword)";
         }
@@ -97,17 +110,28 @@ public class JDBCCredentialVaultRepository extends SimpleJdbcDaoSupport implemen
 
     @Override
     protected JdbcTemplate createJdbcTemplate(DataSource dataSource) {
-        Properties dbCredentials = new Properties();
         try {
-            dbCredentials.load(new FileReader(DB_CREDENTIAL_FILE));
-        } catch (IOException e) {
-            // RuntimeException will be thrown
-            dbCredentialFileNotConfigured(dbCredentials);
+            log.debug("username: {}",BeanUtils.getProperty(dataSource, "username"));
+            log.debug("password: {}",BeanUtils.getProperty(dataSource, "password"));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
-
-        setDbUserCredential(dataSource, dbCredentials);
-
         return super.createJdbcTemplate(dataSource);
+//        Properties dbCredentials = new Properties();
+//        try {
+//            dbCredentials.load(new FileReader(dbCredentialFile));
+//        } catch (IOException e) {
+//            // RuntimeException will be thrown
+//            dbCredentialFileNotConfigured(dbCredentials);
+//        }
+//
+//        setDbUserCredential(dataSource, dbCredentials);
+//
+//        return super.createJdbcTemplate(dataSource);
     }
 
     private void setDbUserCredential(DataSource dataSource, Properties dbCredentials) {
@@ -116,13 +140,13 @@ public class JDBCCredentialVaultRepository extends SimpleJdbcDaoSupport implemen
 
         String clearPwd = null;
         try {
-            clearPwd = CryptoUtils.decrypt(encryptedPwd, new File(CREDENTIAL_VAULT_KEY_FILE));
+            clearPwd = CryptoUtils.decrypt(encryptedPwd, new File(credentialVaultKeyFile));
 
             BeanUtils.setProperty(dataSource, "username", user);
             BeanUtils.setProperty(dataSource, "password", clearPwd);
         } catch (Exception e) {
             e.printStackTrace();
-            throw new RuntimeException("The db-credential has to be configured", e);
+            throw new RuntimeException("The database login-credential has to be configured", e);
         }
     }
 
@@ -130,10 +154,10 @@ public class JDBCCredentialVaultRepository extends SimpleJdbcDaoSupport implemen
         dbCredentials.setProperty("user", "<db-username>");
         dbCredentials.setProperty("password", "<encrypted db-password> - use your cv.key to encrypt the password");
         try {
-            dbCredentials.store(new FileWriter(DB_CREDENTIAL_FILE), "");
+            dbCredentials.store(new FileWriter(dbCredentialFile), "");
         } catch (IOException e1) {
-            log.error("Could not access db-credential properties file {}", DB_CREDENTIAL_FILE);
+            log.error("Could not access database login-credential properties file {}", dbCredentialFile);
         }
-        throw new RuntimeException("The db-credential properties file has to be configured");
+        throw new RuntimeException("The database login-credential properties file has to be configured");
     }
 }
