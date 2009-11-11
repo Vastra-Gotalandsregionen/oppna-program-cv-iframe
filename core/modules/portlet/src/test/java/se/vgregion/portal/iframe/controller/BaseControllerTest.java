a8e480e0a670545b39cac39a280ddcd87ca8c646
wmdaros <wmdaros@47b682a8-c94b-11de-9e96-3193d784f102>
2009-11-11 15:42:59 +0000

    UnitTest for view controller

---
+++ core/modules/portlet/src/test/java/se/vgregion/portal/iframe/controller/BaseControllerTest.java
@@ -0,0 +1,24 @@
+package se.vgregion.portal.iframe.controller;
+
+import javax.portlet.PortletPreferences;
+import javax.portlet.ReadOnlyException;
+
+/**
+ * This action do that and that, if it has something special it is.
+ *
+ * @author <a href="mailto:david.rosell@redpill-linpro.com">David Rosell</a>
+ */
+public class BaseControllerTest {
+    protected void initPortletPreferences(PortletPreferences prefs) throws ReadOnlyException {
+        prefs.setValue("site-key", "test-key");
+        prefs.setValue("src", "test-src");
+        prefs.setValue("relative", "false");
+        prefs.setValue("auth", "true");
+        prefs.setValue("auth-type", "form"); // basic or form
+        prefs.setValue("form-method", "post"); // get or post
+        prefs.setValue("user-name-field", "username");
+        prefs.setValue("password-field", "password");
+        prefs.setValue("hidden-variables", "test1=hidden1;test2=hidden2");
+        prefs.setValue("html-attributes", "html1=apa\nhtml2=bepa");
+    }
+}
---
+++ core/modules/portlet/src/test/java/se/vgregion/portal/iframe/controller/CVViewControllerTest.java
@@ -0,0 +1,376 @@
+package se.vgregion.portal.iframe.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.springframework.mock.web.portlet.*;
+import org.springframework.ui.ModelMap;
+import se.vgregion.portal.iframe.model.UserSiteCredential;
+import se.vgregion.portal.iframe.model.Credential;
+import se.vgregion.portal.repository.CredentialVaultRepository;
+
+import javax.portlet.*;
+import java.util.Map;
+import java.util.HashMap;
+
+/**
+ * This action do that and that, if it has something special it is.
+ *
+ * @author <a href="mailto:david.rosell@redpill-linpro.com">David Rosell</a>
+ */
+public class CVViewControllerTest extends BaseControllerTest {
+    CVViewController controller;
+
+    @Before
+    public void setUp() {
+        controller = new CVViewController();
+        controller.setCredentialVaultRepository(new TestStubCredentialVaultRepository());
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testShowView() throws ReadOnlyException {
+        PortletPreferences prefs = new MockPortletPreferences();
+        initPortletPreferences(prefs);
+
+        RenderRequest mockReq = new MockRenderRequest(PortletMode.VIEW);
+        RenderResponse mockResp = new MockRenderResponse();
+        ModelMap model = new ModelMap();
+
+        String response = controller.showView(prefs, mockReq, mockResp, model);
+        assertEquals("userCredentialForm", response);
+    }
+
+    @Test
+    public void testShowView_NoAuth() throws ReadOnlyException {
+        PortletPreferences prefs = new MockPortletPreferences();
+        initPortletPreferences(prefs);
+        prefs.setValue("auth", "false");
+
+        RenderRequest mockReq = new MockRenderRequest(PortletMode.VIEW);
+        RenderResponse mockResp = new MockRenderResponse();
+        ModelMap model = new ModelMap();
+
+        String response = controller.showView(prefs, mockReq, mockResp, model);
+        assertEquals("view", response);
+
+        // Test model
+        assertEquals("test-src", model.get("iFrameSrc"));
+        assertEquals("test-src", model.get("baseSrc"));
+        assertEquals("300", model.get("iFrameHeight"));
+        assertEquals("0", model.get("border"));
+        assertEquals("#000000", model.get("bordercolor"));
+        assertEquals("0", model.get("frameborder"));
+        assertEquals("600", model.get("height-maximized"));
+        assertEquals("300", model.get("height-normal"));
+        assertEquals("0", model.get("hspace"));
+        assertEquals("auto", model.get("scrolling"));
+        assertEquals("0", model.get("vspace"));
+        assertEquals("100%", model.get("width"));
+    }
+
+    @Test
+    public void testShowView_AuthNoSiteUser() throws ReadOnlyException {
+        PortletPreferences prefs = new MockPortletPreferences();
+        initPortletPreferences(prefs);
+        prefs.setValue("auth", "true");
+
+        RenderRequest mockReq = new MockRenderRequest(PortletMode.VIEW);
+        Map<String, String> userInfo = new HashMap<String, String>();
+        userInfo.put(PortletRequest.P3PUserInfos.USER_LOGIN_ID.toString(), "no-credential-user");
+        mockReq.setAttribute(PortletRequest.USER_INFO, userInfo);
+
+        RenderResponse mockResp = new MockRenderResponse();
+        ModelMap model = new ModelMap();
+
+        String response = controller.showView(prefs, mockReq, mockResp, model);
+        assertEquals("userCredentialForm", response);
+    }
+
+    @Test
+    public void testShowView_AuthSiteUserExistWrongSiteKey() throws ReadOnlyException {
+        PortletPreferences prefs = new MockPortletPreferences();
+        initPortletPreferences(prefs);
+        prefs.setValue("auth", "true");
+
+        RenderRequest mockReq = (RenderRequest) initPortletRequest(new MockRenderRequest());
+
+        RenderResponse mockResp = new MockRenderResponse();
+        ModelMap model = new ModelMap();
+
+        String response = controller.showView(prefs, mockReq, mockResp, model);
+        assertEquals("userCredentialForm", response);
+    }
+
+    @Test
+    public void testShowView_AuthSiteUserExistSiteKeyExistBasic() throws ReadOnlyException {
+        PortletPreferences prefs = new MockPortletPreferences();
+        initPortletPreferences(prefs);
+        prefs.setValue("src", "http://www.google.com");
+        prefs.setValue("auth", "true");
+        prefs.setValue("auth-type", "basic");
+        prefs.setValue("site-key", "test-site-key");
+
+        RenderRequest mockReq = (RenderRequest) initPortletRequest(new MockRenderRequest());
+
+        RenderResponse mockResp = new MockRenderResponse();
+        ModelMap model = new ModelMap();
+
+        String response = controller.showView(prefs, mockReq, mockResp, model);
+        assertEquals("view", response);
+
+        UserSiteCredential siteCredential = (UserSiteCredential) model.get("siteCredential");
+        assertEquals("test-user", siteCredential.getUid());
+        assertEquals("test-site-key", siteCredential.getSiteKey());
+        assertEquals("test-site-user", siteCredential.getSiteUser());
+        assertEquals("test-site-password", siteCredential.getSitePassword());
+
+        // Test model
+        assertEquals("http://"+siteCredential.getSiteUser()+":"+siteCredential.getSitePassword()+"@www.google.com", model.get("iFrameSrc"));
+        assertEquals("http://"+siteCredential.getSiteUser()+":"+siteCredential.getSitePassword()+"@www.google.com", model.get("baseSrc"));
+        assertEquals("300", model.get("iFrameHeight"));
+        assertEquals("0", model.get("border"));
+        assertEquals("#000000", model.get("bordercolor"));
+        assertEquals("0", model.get("frameborder"));
+        assertEquals("600", model.get("height-maximized"));
+        assertEquals("300", model.get("height-normal"));
+        assertEquals("0", model.get("hspace"));
+        assertEquals("auto", model.get("scrolling"));
+        assertEquals("0", model.get("vspace"));
+        assertEquals("100%", model.get("width"));
+    }
+
+    @Test
+    public void testShowView_AuthSiteUserExistSiteKeyExistForm() throws ReadOnlyException {
+        PortletPreferences prefs = new MockPortletPreferences();
+        initPortletPreferences(prefs);
+        prefs.setValue("src", "http://www.google.com");
+        prefs.setValue("auth", "true");
+        prefs.setValue("auth-type", "form");
+        prefs.setValue("site-key", "test-site-key");
+
+        MockRenderRequest mockReq = new MockRenderRequest(PortletMode.VIEW);
+        RenderRequest req = (MockRenderRequest) initPortletRequest(mockReq);
+
+        MockRenderResponse resp = new TestStubMockRenderResponse();
+
+        ModelMap model = new ModelMap();
+
+        String response = controller.showView(prefs, req, resp, model);
+        assertEquals("view", response);
+
+        UserSiteCredential siteCredential = (UserSiteCredential) model.get("siteCredential");
+        assertEquals("test-user", siteCredential.getUid());
+        assertEquals("test-site-key", siteCredential.getSiteKey());
+        assertEquals("test-site-user", siteCredential.getSiteUser());
+        assertEquals("test-site-password", siteCredential.getSitePassword());
+
+        // Test model
+        assertEquals("http://localhost/mockportlet?resourceID=resourceId", model.get("iFrameSrc"));
+        assertEquals("http://localhost/", model.get("baseSrc"));
+        assertEquals("300", model.get("iFrameHeight"));
+        assertEquals("0", model.get("border"));
+        assertEquals("#000000", model.get("bordercolor"));
+        assertEquals("0", model.get("frameborder"));
+        assertEquals("600", model.get("height-maximized"));
+        assertEquals("300", model.get("height-normal"));
+        assertEquals("0", model.get("hspace"));
+        assertEquals("auto", model.get("scrolling"));
+        assertEquals("0", model.get("vspace"));
+        assertEquals("100%", model.get("width"));
+    }
+
+    @Test
+    public void testShowView_WindowStateMaximized() throws ReadOnlyException {
+        PortletPreferences prefs = new MockPortletPreferences();
+        initPortletPreferences(prefs);
+        prefs.setValue("src", "http://www.google.com");
+        prefs.setValue("auth", "true");
+        prefs.setValue("auth-type", "form");
+        prefs.setValue("site-key", "test-site-key");
+
+        MockRenderRequest mockReq = new MockRenderRequest(PortletMode.VIEW);
+        mockReq = (MockRenderRequest)initPortletRequest(mockReq);
+
+        MockRenderResponse mockResp = new TestStubMockRenderResponse();
+        mockResp.createResourceURL();
+
+        ModelMap model = new ModelMap();
+
+        // NORMAL
+        mockReq.setWindowState(WindowState.NORMAL);
+        controller.showView(prefs, mockReq, mockResp, model);
+        assertEquals("300", model.get("iFrameHeight"));
+
+        // MAXIMIZED
+        mockReq.setWindowState(WindowState.MAXIMIZED);
+        controller.showView(prefs, mockReq, mockResp, model);
+        assertEquals("600", model.get("iFrameHeight"));
+    }
+
+    private PortletRequest initPortletRequest(PortletRequest req) {
+        Map<String, String> userInfo = new HashMap<String, String>();
+        userInfo.put(PortletRequest.P3PUserInfos.USER_LOGIN_ID.toString(), "test-user");
+        req.setAttribute(PortletRequest.USER_INFO, userInfo);
+        return req;
+    }
+
+    @Test
+    public void testChangeVaultCredentials_NoUserSiteCredential() throws ReadOnlyException {
+        PortletPreferences prefs = new MockPortletPreferences();
+        initPortletPreferences(prefs);
+
+        MockRenderRequest mockReq = new MockRenderRequest(PortletMode.VIEW);
+        RenderRequest req = (RenderRequest)initPortletRequest(mockReq);
+        ModelMap model = new ModelMap();
+
+        String response = controller.changeVaultCredentials(prefs, req, model);
+        assertEquals("userCredentialForm", response);
+
+        Credential credential = (Credential) model.get("credential");
+        assertNotNull(credential);
+        assertEquals("test-key", credential.getSiteKey());
+        assertEquals("test-src", credential.getSrc());
+        assertEquals(Boolean.FALSE, credential.isRelative());
+        assertEquals(Boolean.TRUE, credential.isAuth());
+        assertEquals("form", credential.getAuthType());
+        assertEquals("post", credential.getFormMethod());
+        assertEquals("username", credential.getSiteUserNameField());
+        assertEquals("password", credential.getSitePasswordField());
+        assertEquals("test1=hidden1;test2=hidden2", credential.getHiddenVariables());
+        assertEquals("html1=apa\nhtml2=bepa", credential.getHtmlAttributes());
+
+        UserSiteCredential siteCredential = (UserSiteCredential) model.get("siteCredential");
+        assertNotNull(siteCredential);
+
+    }
+
+    @Test
+    public void testChangeVaultCredentials_NoAuth() throws ReadOnlyException {
+        PortletPreferences prefs = new MockPortletPreferences();
+        initPortletPreferences(prefs);
+        prefs.setValue("auth", "false");
+
+        MockRenderRequest mockReq = new MockRenderRequest(PortletMode.VIEW);
+        RenderRequest req = (RenderRequest)initPortletRequest(mockReq);
+        ModelMap model = new ModelMap();
+
+        String response = controller.changeVaultCredentials(prefs, req, model);
+        assertEquals("view", response);
+
+        Credential credential = (Credential) model.get("credential");
+        assertNotNull(credential);
+        assertEquals("test-src", credential.getSrc());
+        assertEquals(Boolean.FALSE, credential.isRelative());
+        assertEquals(Boolean.FALSE, credential.isAuth());
+        assertEquals("html1=apa\nhtml2=bepa", credential.getHtmlAttributes());
+    }
+
+    @Test
+    public void testShowProxyForm_AuthForm() throws ReadOnlyException {
+        PortletPreferences prefs = new MockPortletPreferences();
+        initPortletPreferences(prefs);
+
+        ResourceRequest mockReq = new MockResourceRequest();
+        ResourceRequest req = (ResourceRequest) initPortletRequest(mockReq);
+
+        ModelMap model = new ModelMap();
+
+        String result = controller.showProxyForm(prefs, req, model);
+
+        assertEquals("proxyLoginForm", result);
+
+        Credential credential = (Credential) model.get("credential");
+        assertNotNull(credential);
+
+        UserSiteCredential siteCredential = (UserSiteCredential)model.get("siteCredential");
+        assertNotNull(siteCredential);
+    }
+
+    @Test
+    public void testShowProxyForm_NoAuthForm() throws ReadOnlyException {
+        PortletPreferences prefs = new MockPortletPreferences();
+        initPortletPreferences(prefs);
+        prefs.setValue("auth", "false");
+
+        ResourceRequest mockReq = new MockResourceRequest();
+        ResourceRequest req = (ResourceRequest) initPortletRequest(mockReq);
+
+        ModelMap model = new ModelMap();
+
+        String result = controller.showProxyForm(prefs, req, model);
+
+        assertEquals("view", result);
+    }
+
+    @Test
+    public void testShowProxyForm_AuthNoForm() throws ReadOnlyException {
+        PortletPreferences prefs = new MockPortletPreferences();
+        initPortletPreferences(prefs);
+        prefs.setValue("auth-type", "basic");
+
+        ResourceRequest mockReq = new MockResourceRequest();
+        ResourceRequest req = (ResourceRequest) initPortletRequest(mockReq);
+
+        ModelMap model = new ModelMap();
+
+        String result = controller.showProxyForm(prefs, req, model);
+
+        assertEquals("view", result);
+    }
+
+    @Test
+    public void testShowProxyForm_NoAuthNoForm() throws ReadOnlyException {
+        PortletPreferences prefs = new MockPortletPreferences();
+        initPortletPreferences(prefs);
+        prefs.setValue("auth", "false");
+        prefs.setValue("auth-type", "basic");
+
+        ResourceRequest mockReq = new MockResourceRequest();
+        ResourceRequest req = (ResourceRequest) initPortletRequest(mockReq);
+
+        ModelMap model = new ModelMap();
+
+        String result = controller.showProxyForm(prefs, req, model);
+
+        assertEquals("view", result);
+    }
+
+    @Test
+    public void testStoreUserCredential() {
+        // TODO: How test this?
+    }
+
+    class TestStubCredentialVaultRepository implements CredentialVaultRepository {
+
+        public UserSiteCredential getUserSiteCredential(String uid, String siteKey) {
+            if ("test-user".equals(uid) && "test-site-key".equals(siteKey)) {
+                UserSiteCredential siteCredential = new UserSiteCredential("test-user", "test-site-key");
+                siteCredential.setSiteUser("test-site-user");
+                siteCredential.setSitePassword("test-site-password");
+
+                return siteCredential;
+            } else {
+                return null;
+            }
+        }
+
+        public void addUserSiteCredential(UserSiteCredential siteCredential) {
+            //To change body of implemented methods use File | Settings | File Templates.
+        }
+    }
+
+    class TestStubMockRenderResponse extends MockRenderResponse {
+        @Override
+        public ResourceURL createResourceURL() {
+            MockResourceURL mockResourceURL = new MockResourceURL();
+            mockResourceURL.setResourceID("resourceId");
+
+            return mockResourceURL;
+        }
+    }
+}
