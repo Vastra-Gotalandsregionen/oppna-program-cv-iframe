a294fc37f1a50b27b7521f658495691a166d9a2d
asse.hemma <asse.hemma@47b682a8-c94b-11de-9e96-3193d784f102>
2010-09-28 14:51:17 +0000

    Added test, corrected failing test

--- trunk/core/composites/svc/src/main/java/se/vgregion/portal/csiframe/service/UserSiteCredentialServiceImpl.java
+++ trunk/core/composites/svc/src/main/java/se/vgregion/portal/csiframe/service/UserSiteCredentialServiceImpl.java
@@ -1,7 +1,5 @@
 package se.vgregion.portal.csiframe.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,8 +9,6 @@ import se.vgregion.portal.csiframe.domain.UserSiteCredentialRepository;
 
 @Service
 public class UserSiteCredentialServiceImpl implements UserSiteCredentialService {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(UserSiteCredentialServiceImpl.class);
 
     @Autowired
     private UserSiteCredentialRepository userSiteCredentialRepository;
