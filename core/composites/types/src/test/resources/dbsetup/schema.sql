179e78cbc028ecef16d305de3c9454ff4f6c3958
wmdaros <wmdaros@47b682a8-c94b-11de-9e96-3193d784f102>
2011-09-24 00:54:53 +0000

    - Tests added - Cleanup

--- core/composites/types/src/test/resources/dbsetup/schema.sql
+++ core/composites/types/src/test/resources/dbsetup/schema.sql
@@ -10,3 +10,19 @@ CREATE TABLE vgr_user_site_credential
   CONSTRAINT vgr_user_site_credential_pkey PRIMARY KEY (id),
   CONSTRAINT vgr_user_site_credential_uid_key UNIQUE (uid, site_key)
 );
+
+DROP TABLE vgr_site_key;
+
+CREATE TABLE vgr_site_key
+(
+  id bigint NOT NULL,
+  active boolean,
+  description character varying(1024),
+  site_key character varying(50) NOT NULL,
+  title character varying(50) NOT NULL,
+  screennameonly boolean,
+  suggestscreenname boolean,
+  CONSTRAINT vgr_site_key_pkey PRIMARY KEY (id),
+  CONSTRAINT vgr_site_key_site_key_key UNIQUE (site_key),
+  CONSTRAINT vgr_site_key_title_key UNIQUE (title)
+);
