7a6252d79fef79cdd40252eb75129e0415b69d3a
wmdaros <wmdaros@47b682a8-c94b-11de-9e96-3193d784f102>
2011-12-07 09:15:28 +0000

    [maven-release-plugin]  copy for tag cs-iframe-system-2.1

---
+++ tags/cs-iframe-system-2.1/core/modules/portlet/src/main/java/se/vgregion/portal/admin/controller/ViewController.java
@@ -0,0 +1,123 @@
+package se.vgregion.portal.admin.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.portlet.bind.annotation.ActionMapping;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
+import se.vgregion.portal.cs.domain.SiteKey;
+import se.vgregion.portal.cs.domain.UserSiteCredential;
+import se.vgregion.portal.cs.service.CredentialService;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.RenderRequest;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+
+import static javax.portlet.PortletRequest.P3PUserInfos.USER_LOGIN_ID;
+import static javax.portlet.PortletRequest.USER_INFO;
+
+/**
+ * @author <a href="mailto:david.rosell@redpill-linpro.com">David Rosell</a>
+ */
+@Controller
+@RequestMapping("VIEW")
+public class ViewController {
+    private static final Logger LOG = LoggerFactory.getLogger(ViewController.class);
+
+    @Autowired
+    private CredentialService credentialService;
+
+    @RenderMapping
+    public String showView(RenderRequest req, Model model) {
+        String uid = lookupUid(req);
+
+        List<CredentialSiteKeyFormBean> credentials = prepareCredentials(uid);
+
+        model.addAttribute("userCredentials", credentials);
+
+        return "view";
+    }
+
+    @ActionMapping("updateCredential")
+    public void updateCredential(@ModelAttribute UserSiteCredential userCredential, Model model) {
+        try {
+            credentialService.save(userCredential);
+
+            model.addAttribute("saveAction", userCredential.getSiteKey());
+        } catch (Exception ex) {
+            model.addAttribute("saveActionFailed", userCredential.getSiteKey());
+        }
+    }
+
+    @ActionMapping("refreshCredential")
+    public void refreshCredential() {
+    }
+
+    @ActionMapping("removeCredential")
+    public void deleteCredentials(@RequestParam("userCredentialId") Long userCredentialId, Model model) {
+        UserSiteCredential siteCredential = null;
+        try {
+            siteCredential = credentialService.getUserSiteCredential(userCredentialId);
+
+            credentialService.removeUserSiteCredential(userCredentialId);
+
+            model.addAttribute("removeAction", siteCredential.getSiteKey());
+        } catch (Exception ex) {
+            if (siteCredential != null)
+                model.addAttribute("removeActionFailed", siteCredential.getSiteKey());
+            else
+                model.addAttribute("removeActionFailed", true);
+        }
+    }
+
+    private List<CredentialSiteKeyFormBean> prepareCredentials(String uid) {
+        List<UserSiteCredential> userCredentials =
+                new UserSiteCredentialHelper(credentialService.getAllSiteCredentials(uid)).get();
+
+        List<SiteKey> siteKeys = new SiteKeyHelper(credentialService.getAllSiteKeys())
+                .filterActive().get();
+
+        List<CredentialSiteKeyFormBean> credentials = new ArrayList<CredentialSiteKeyFormBean>();
+        for (UserSiteCredential credential : userCredentials) {
+            for (SiteKey siteKey : siteKeys) {
+                if (credential.getSiteKey().equals(siteKey.getSiteKey())) {
+                    credentials.add(new CredentialSiteKeyFormBean(credential, siteKey));
+                    break;
+                }
+            }
+        }
+        // Sort credentials by SiteKey title
+        Collections.sort(credentials, new CredentialSiteKeyFormBeanComparator());
+
+        return credentials;
+    }
+
+    private static class CredentialSiteKeyFormBeanComparator implements Comparator<CredentialSiteKeyFormBean>,
+            Serializable {
+        private static final long serialVersionUID = 5880281199565660993L;
+
+        @Override
+        public int compare(CredentialSiteKeyFormBean one, CredentialSiteKeyFormBean other) {
+            return one.getSiteKey().getTitle().toLowerCase().compareTo(other.getSiteKey().getTitle().toLowerCase());
+        }
+    }
+
+    private String lookupUid(PortletRequest req) {
+        Map<String, ?> userInfo = (Map<String, ?>) req.getAttribute(USER_INFO);
+        String userId = "";
+        if (userInfo != null && userInfo.get(USER_LOGIN_ID.toString()) != null) {
+            userId = (String) userInfo.get(USER_LOGIN_ID.toString());
+        }
+        return userId;
+    }
+}
---
+++ tags/cs-iframe-system-2.1/core/modules/portlet/src/main/java/se/vgregion/portal/iframe/controller/CSViewController.java
@@ -0,0 +1,345 @@
+/**
+ * Copyright 2010 Västra Götalandsregionen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *   Boston, MA 02111-1307  USA
+ *
+ */
+
+package se.vgregion.portal.iframe.controller;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Enumeration;
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.PortletPreferences;
+import javax.portlet.PortletRequest;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import javax.portlet.ResourceRequest;
+import javax.portlet.WindowState;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.bind.annotation.ActionMapping;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
+import org.springframework.web.portlet.bind.annotation.ResourceMapping;
+import org.springframework.web.portlet.util.PortletUtils;
+
+import se.vgregion.portal.cs.domain.SiteKey;
+import se.vgregion.portal.cs.domain.UserSiteCredential;
+import se.vgregion.portal.cs.service.CredentialService;
+import se.vgregion.portal.iframe.model.PortletConfig;
+
+/**
+ * @author <a href="mailto:david.rosell@redpill-linpro.com">David Rosell</a>
+ */
+@Controller
+@RequestMapping("VIEW")
+public class CSViewController {
+    private Logger log = LoggerFactory.getLogger(CSViewController.class);
+
+    @Autowired
+    private CredentialService credentialService;
+
+    /**
+     * Main controllermethod. Handling of user-sitecredential availability and iFrame source linking
+     * 
+     * @param prefs
+     *            - protlet preferences
+     * @param req
+     *            - request
+     * @param resp
+     *            - response
+     * @param model
+     *            - model
+     * @return view
+     */
+    @RenderMapping
+    public String showView(PortletPreferences prefs, RenderRequest req, RenderResponse resp, ModelMap model) {
+        PortletConfig portletConfig = PortletConfig.getInstance(prefs);
+        log.debug("Creds: {}", portletConfig);
+
+        SiteKey siteKey = credentialService.getSiteKey(portletConfig.getSiteKey());
+        model.addAttribute("siteKey", siteKey);
+
+        UserSiteCredential siteCredential = new UserSiteCredential();
+        if (siteKey != null && !credentialsAvailable(req, model, portletConfig, siteCredential, siteKey)) {
+            model.addAttribute("portletConfig", portletConfig);
+            return "userCredentialForm";
+        }
+
+        String iFrameSrc = prepareView(resp, req, portletConfig, siteCredential);
+        String preIFrameSrc;
+        if (StringUtils.isNotBlank(portletConfig.getPreIFrameAction())) {
+            preIFrameSrc = portletConfig.getPreIFrameAction();
+        } else {
+            preIFrameSrc = iFrameSrc;
+        }
+        String baseSrc = getBaseSrc(iFrameSrc);
+
+        String iFrameHeight = getIFrameHeight(req, portletConfig);
+
+        model.addAttribute("iFrameSrc", iFrameSrc);
+        model.addAttribute("preIFrameSrc", preIFrameSrc);
+        model.addAttribute("baseSrc", baseSrc);
+        model.addAttribute("iFrameHeight", iFrameHeight);
+        model.addAttribute("border", portletConfig.getHtmlAttribute("border", "0"));
+        model.addAttribute("bordercolor", portletConfig.getHtmlAttribute("bordercolor", "#000000"));
+        model.addAttribute("frameborder", portletConfig.getHtmlAttribute("frameborder", "0"));
+        model.addAttribute("height-maximized", portletConfig.getHtmlAttribute("height-maximized", "600"));
+        model.addAttribute("height-normal", portletConfig.getHtmlAttribute("height-normal", "300"));
+        model.addAttribute("hspace", portletConfig.getHtmlAttribute("hspace", "0"));
+        model.addAttribute("scrolling", portletConfig.getHtmlAttribute("scrolling", "auto"));
+        model.addAttribute("vspace", portletConfig.getHtmlAttribute("vspace", "0"));
+        model.addAttribute("width", portletConfig.getHtmlAttribute("width", "100%"));
+        String linkDisplay = portletConfig.isAuth() ? "display:block;" : "display:none;";
+        model.addAttribute("link_display", linkDisplay);
+        model.addAttribute("myPortletConfig", portletConfig);
+
+        return "view";
+    }
+
+    /**
+     * Credential view handleing.
+     * 
+     * @param prefs
+     *            - portlet preferences
+     * @param req
+     *            - request
+     * @param model
+     *            - model
+     * @return view
+     */
+    @RenderMapping(params = "action=changeVaultCredentials")
+    public String changeVaultCredentials(PortletPreferences prefs, RenderRequest req, ModelMap model) {
+        PortletConfig portletConfig = PortletConfig.getInstance(prefs);
+        model.addAttribute("portletConfig", portletConfig);
+        if (!portletConfig.isAuth()) {
+            return "view";
+        }
+
+        SiteKey siteKey = credentialService.getSiteKey(portletConfig.getSiteKey());
+        model.addAttribute("siteKey", siteKey);
+
+        UserSiteCredential siteCredential = new UserSiteCredential();
+        credentialsAvailable(req, model, portletConfig, siteCredential, siteKey);
+
+        return "userCredentialForm";
+    }
+
+    /**
+     * Prepare proxyLoginForm.jsp for form-based authentication.
+     * 
+     * @param model
+     *            - model
+     * @param req
+     *            - request
+     * @param prefs
+     *            - portlet preferences
+     * @return view
+     * @throws URISyntaxException
+     */
+    @ResourceMapping
+    public String showProxyForm(PortletPreferences prefs, ResourceRequest req, ModelMap model)
+            throws URISyntaxException {
+        PortletConfig portletConfig = se.vgregion.portal.iframe.model.PortletConfig.getInstance(prefs);
+        model.addAttribute("portletConfig", portletConfig);
+        if (!portletConfig.isAuth() || !"form".equals(portletConfig.getAuthType())) {
+            return "view";
+        }
+
+        SiteKey siteKey = credentialService.getSiteKey(portletConfig.getSiteKey());
+
+        UserSiteCredential siteCredential = new UserSiteCredential();
+        credentialsAvailable(req, model, portletConfig, siteCredential, siteKey);
+
+        URI src = new URI(portletConfig.getSrc());
+
+        String proxyFormAction;
+        if (portletConfig.getFormAction() == null || portletConfig.getFormAction().length() < 1) {
+            proxyFormAction = src.toString();
+        } else {
+            proxyFormAction = src.resolve(portletConfig.getFormAction()).toString();
+        }
+
+        model.addAttribute("proxyFormAction", proxyFormAction);
+        debug(req, proxyFormAction);
+
+        return "proxyLoginForm";
+    }
+
+    private void debug(ResourceRequest req, String proxyFormAction) {
+        if (log.isDebugEnabled()) {
+            log.debug("ProxyFormAction: {}", proxyFormAction);
+
+            log.debug("Request attributes");
+            Enumeration attrs = req.getAttributeNames();
+            while (attrs.hasMoreElements()) {
+                String attr = attrs.nextElement().toString();
+                log.debug("Request attribute: {} : {}", attr, req.getAttribute(attr));
+            }
+
+            log.debug("Request parameters");
+            Enumeration params = req.getParameterNames();
+            while (params.hasMoreElements()) {
+                String param = params.nextElement().toString();
+                log.debug("Request parameters: {} : {}", param, req.getParameterValues(param));
+            }
+        }
+    }
+
+    /**
+     * Store User-SiteCredentials in the Credential-Vault. Action posted from userCredentailForm.jsp
+     * 
+     * @param siteCredential
+     *            - credential
+     * @param req
+     *            - action request to handle cancel
+     */
+    @Transactional
+    @ActionMapping
+    public void storeUserCredential(@ModelAttribute("siteCredential") UserSiteCredential siteCredential,
+            ActionRequest req) {
+
+        if (StringUtils.isBlank(siteCredential.getUid())) {
+            throw new RuntimeException("ERROR: Unknown user. Cannot store credential.");
+        }
+
+        if (StringUtils.isBlank(siteCredential.getSiteKey())) {
+            throw new RuntimeException("CONFIGURATION ERROR: No site-key given. Cannot store user credential.");
+        }
+        if (!PortletUtils.hasSubmitParameter(req, "_cancel")) {
+            UserSiteCredential entToStore = credentialService.getUserSiteCredential(
+                    siteCredential.getUid(), siteCredential.getSiteKey());
+            if (entToStore == null || entToStore.getId() == null) {
+                entToStore = siteCredential;
+            } else {
+                entToStore.setSiteUser(siteCredential.getSiteUser());
+                entToStore.setSitePassword(siteCredential.getSitePassword());
+            }
+            // credentialStoreRepository.save(siteCredential);
+            credentialService.save(entToStore);
+            // log.debug("storeUserCredential: {}", siteCredential);
+        }
+    }
+
+    private String prepareView(RenderResponse resp, RenderRequest req, PortletConfig portletConfig, UserSiteCredential siteCredential) {
+        String iFrameSrc = getDefaultTarget();
+
+        String src = portletConfig.getSrc();
+
+        if (portletConfig.isAuth()) {
+            if (portletConfig.getAuthType().equals("basic")) {
+                // goto src with basic authentication
+                int pos = src.indexOf("://");
+
+                String protocol = src.substring(0, pos + 3);
+                String url = src.substring(pos + 3, src.length());
+
+                src = protocol + siteCredential.getSiteUser() + ":" + siteCredential.getSitePassword() + "@" + url;
+            } else {
+                // goto proxy for form login
+                src = resp.createResourceURL().toString();
+            }
+        }
+
+        if (src.length() > 0) {
+            iFrameSrc = src;
+        }
+
+        if (portletConfig.isUserLoggedIn()) {
+            if (iFrameSrc.indexOf("?") == -1) {
+                iFrameSrc += "?userId="+lookupUid(req);
+            } else {
+                iFrameSrc += "&userId="+lookupUid(req);
+            }
+        }
+
+        return iFrameSrc;
+    }
+
+    private String getDefaultTarget() {
+        return "http://vgregion.se/";
+    }
+
+    private boolean credentialsAvailable(PortletRequest req, ModelMap model, PortletConfig portletConfig,
+            UserSiteCredential returnSiteCredential, SiteKey siteKey) {
+        boolean userSiteCredentialExist = true;
+        if (portletConfig.isAuth()) {
+            String uid = lookupUid(req);
+            returnSiteCredential.setUid(uid);
+            returnSiteCredential.setSiteKey(portletConfig.getSiteKey());
+            if (siteKey.getScreenNameOnly() || siteKey.getSuggestScreenName()) {
+                returnSiteCredential.setSiteUser(uid);
+            }
+
+            UserSiteCredential siteCredential = credentialService.getUserSiteCredential(uid,
+                    portletConfig.getSiteKey());
+
+            if (siteCredential != null) {
+                returnSiteCredential.setId(siteCredential.getId());
+                if (!siteKey.getScreenNameOnly()) {
+                    returnSiteCredential.setSiteUser(siteCredential.getSiteUser());
+                }
+                returnSiteCredential.setSitePassword(siteCredential.getSitePassword());
+            } else {
+                userSiteCredentialExist = false;
+            }
+            model.addAttribute("siteCredential", returnSiteCredential);
+        }
+        return userSiteCredentialExist;
+    }
+
+    private String lookupUid(PortletRequest req) {
+        Map<String, ?> userInfo = (Map<String, ?>) req.getAttribute(PortletRequest.USER_INFO);
+        String userId;
+        if (userInfo != null) {
+            userId = (String) userInfo.get(PortletRequest.P3PUserInfos.USER_LOGIN_ID.toString());
+        } else {
+            userId = "";
+        }
+        return userId;
+    }
+
+    private String getBaseSrc(String iFrameSrc) {
+        String baseSrc = iFrameSrc;
+        int lastSlashPos = iFrameSrc.substring(7).lastIndexOf("/");
+        if (lastSlashPos != -1) {
+            baseSrc = iFrameSrc.substring(0, lastSlashPos + 8);
+        }
+        return baseSrc;
+    }
+
+    private String getIFrameHeight(RenderRequest req, PortletConfig portletConfig) {
+        WindowState windowState = req.getWindowState();
+        String iFrameHeight;
+        if (windowState.equals(WindowState.NORMAL)) {
+            iFrameHeight = portletConfig.getHtmlAttribute("height-normal", "300");
+        } else {
+            iFrameHeight = portletConfig.getHtmlAttribute("height-maximized", "600");
+        }
+        return iFrameHeight;
+    }
+
+}
