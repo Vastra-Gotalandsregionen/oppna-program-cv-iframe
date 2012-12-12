bfcd32366dbc9b15c56760e464e096f8d04a3661
patrik.bergstrom@knowit.se <patrik.bergstrom@knowit.se@47b682a8-c94b-11de-9e96-3193d784f102>
2012-12-12 10:26:49 +0000

    remove tag

--- tags/cs-iframe-system-2.9/core/composites/svc/src/main/java/se/vgregion/portal/csiframe/svc/MailInfService.java
+++
@@ -1,7 +0,0 @@
-package se.vgregion.portal.csiframe.svc;
-
-public interface MailInfService {
-
-    String findServerName(String userId);
-
-}
--- tags/cs-iframe-system-2.9/core/modules/application/src/main/java/se/vgregion/portal/cs/migration/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Package containing classes relating to migration of cipher texts.
- */
-package se.vgregion.portal.cs.migration;
-
